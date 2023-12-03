ee05128e47f2fdb66eb718644dbbdb66daf32b54
Hunter Zhuang <101zh@users.noreply.github.com>
2023-12-02 20:16:11 -0800

    chore(setup): Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# For Those Who Want to Contribute!
+- Fork this repository and add your own code
+- Please make sure that it runs correctly for projectStem
+- Comment out any projectStem dependencies when uploading code
+
---
+++ src/test/java/TestTest.java
@@ -0,0 +1,20 @@
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class TestTest {
+    public TestTest() {
+    }
+
+    @Test
+    public void testTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+}
